77a44329ae9b944c30bad8c7c610a143511f71db
salar-ghi <salar.ghi1993@gmail.com>
2024-11-09 01:07:56 +0330

    backend finish almost

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,35 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import cors from "cors"
+import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
 // ROUTE IMPORTS
+import dashboardRoutes from "./routes/dashboardRoutes"
 
 // CONFIGURATION
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
+// app.get("/hello", (req, res) => {
+//     console.log("helo world");
+//     res.send("hello world");
+// });
 
-// SErver
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+app.use("/dashboard", dashboardRoutes);
+
+
+// Server
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
 });
